04972ac87795703b943510627bb53d038a7db537
strange-powers <jv@strange-powers.com>
2017-09-23 13:38:55 +0200

    placed layout file iteration in one function

--- controller/SPExtensionController.php
+++ controller/SPExtensionController.php
@@ -18,12 +18,16 @@ class SPExtensionController {
 	/**
 	 * Gathers data and fills the model object with this data
 	 *
+	 * @class Mage
+	 *
 	 * @param string $extensionName
 	 *
 	 * @return SPExtension
 	 */
 	public function generateExtension($extensionName) {
-		$modelData = array("name"   => $extensionName);
+		$modelData = array(
+			"name"          => $extensionName
+		);
 
 		$modelData["configFile"] = Mage::getBaseDir("etc") . DS . "modules" . DS . $extensionName . ".xml";
 		$modelData["modelPath"] = Mage::getModuleDir("", $extensionName);
@@ -32,10 +36,21 @@ class SPExtensionController {
 		$layoutFiles = $this->gatherLayoutFiles($configFile);
 		$modelData["layoutFiles"] = $layoutFiles;
 
-		$modelData["jsFiles"] = $this->gatherJSFiles($layoutFiles);
+		/* Be careful the lambda function is placed in a loop */
+		$lambdaReturnArr = array(
+			"templateFiles" => array(),
+			"skinFiles"     => array(),
+			"jsFiles"       => array()
+		);
+		$gatheredFiles = $this->loadLayoutFiles($lambdaReturnArr, $layoutFiles, function($xmlParser, $area, &$result) use ($modelData) {
+			$result["templateFiles"]   = array_merge($result["templateFiles"], $this->gatherTemplateFiles($xmlParser, $area));
+			$result["skinFiles"]       = array_merge($result["skinFiles"], $this->gatherSkinFiles($xmlParser, $area));
+			$result["jsFiles"]         = array_merge($result["jsFiles"], $this->gatherJSFiles($xmlParser));
+		});
 
-		$modelData["templateFiles"] = $this->gatherTemplateFiles($layoutFiles);
-		$modelData["skinFiles"] = $this->gatherSkinFiles($layoutFiles);
+		$modelData["templateFiles"] = $this->checkPathsInThemes($gatheredFiles["templateFiles"], "checkForTemplateFile");
+		$modelData["skinFiles"] = $this->checkPathsInThemes($gatheredFiles["skinFiles"], "checkForSkinFile");
+		$modelData["jsFiles"] = $gatheredFiles["jsFiles"];
 
 		return new SPExtension($modelData);
 	}
@@ -43,50 +58,43 @@ class SPExtensionController {
 	/**
 	 * Returns the found template files
 	 *
-	 * @param string $layoutFiles
+	 * @param SPXMLParser $xmlParser
+	 * @param string $area
 	 *
 	 * @return string[]
 	 */
-	private function gatherTemplateFiles($layoutFiles) {
-		$foundPaths = $this->gatherPathsFromLayoutFiles($layoutFiles, array("template"), array("template"));
-
-		return $this->checkPathsInThemes($foundPaths, "checkForTemplateFile");
+	private function gatherTemplateFiles($xmlParser, $area) {
+		return $this->gatherPathsFromLayoutFiles($xmlParser, $area, array("template"), array("template"));
 	}
 
 	/**
 	 * Returns the found skin files
 	 *
-	 * @param array $areaLayoutFiles
+	 * @param SPXMLParser $xmlParser
+	 * @param string $area
 	 *
 	 * @return string[]
 	 */
-	private function gatherSkinFiles($areaLayoutFiles) {
-		$foundPaths = $this->gatherPathsFromLayoutFiles( $areaLayoutFiles, array( "stylesheet", "name" ), array() );
+	private function gatherSkinFiles($xmlParser, $area) {
+		$foundPaths = $this->gatherPathsFromLayoutFiles( $xmlParser, $area, array( "stylesheet", "name" ), array() );
 
-		$xmlParser = new SPXMLParser();
+		$helperNodes = $xmlParser->searchForNodesByAttribute( "helper" );
 
-		foreach ( $areaLayoutFiles as $area => $layoutFiles ) {
-			foreach ( $layoutFiles as $file ) {
-				$xmlParser->load( $file );
-				$helperNodes = $xmlParser->searchForNodesByAttribute( "helper" );
-
-				foreach ( $helperNodes as $helperNode ) {
-					$helperInfo = $helperNode->getAttribute( "helper" );
-					$info = $this->gatherHelperInfo($helperInfo);
-					if ( ! is_null( $info ) && $info !== false ) {
-						if ( is_array( $info ) ) {
-							foreach ( $info as $foundFile ) {
-								array_push( $foundPaths[$area], $foundFile );
-							}
-						} else {
-							array_push( $foundPaths[$area], $info );
-						}
+		foreach ( $helperNodes as $helperNode ) {
+			$helperInfo = $helperNode->getAttribute( "helper" );
+			$info = $this->gatherHelperInfo($helperInfo);
+			if ( ! is_null( $info ) && $info !== false ) {
+				if ( is_array( $info ) ) {
+					foreach ( $info as $foundFile ) {
+						array_push( $foundPaths[$area], $foundFile );
 					}
+				} else {
+					array_push( $foundPaths[$area], $info );
 				}
 			}
 		}
 
-		return $this->checkPathsInThemes($foundPaths, "checkForSkinFile");
+		return $foundPaths;
 	}
 
 	/**
@@ -121,8 +129,14 @@ class SPExtensionController {
 		return $foundFiles;
 	}
 
-	private function gatherJSFiles( $areaLayoutFiles ) {
-		$xmlParser = new SPXMLParser();
+	/**
+	 * Gathers all JS Files
+	 *
+	 * @param SPXMLParser $xmlParser
+	 *
+	 * @return string[]
+	 */
+	private function gatherJSFiles( $xmlParser ) {
 		$gatheredFiles = array();
 
 		$pathCheckFunc = function($file) {
@@ -134,33 +148,28 @@ class SPExtensionController {
 			}
 		};
 
-		foreach ( $areaLayoutFiles as $area => $layoutFiles ) {
-			foreach ( $layoutFiles as $file ) {
-				$xmlParser->load($file);
-				$jsAttributeNodes = $xmlParser->searchForElementByAttributeContainsValue("method", "addJs");
+		$jsAttributeNodes = $xmlParser->searchForElementByAttributeContainsValue("method", "addJs");
 
-				foreach($jsAttributeNodes as $attributeNode) {
-					$scriptTags = $attributeNode->getElementsByTagName("script");
-					foreach($scriptTags as $tag) {
-						$file = null;
+		foreach($jsAttributeNodes as $attributeNode) {
+			$scriptTags = $attributeNode->getElementsByTagName("script");
+			foreach($scriptTags as $tag) {
+				$file = null;
 
-						if($tagAttribute = $tag->getAttribute("helper")) {
-							$file = $this->gatherHelperInfo($tagAttribute);
-						} else if($tagValue = $tag->getElementsByTagName("type")) {
-							if(strpos($tagValue->item(0)->textContent, "skin") === false) {
-								$file = $tag->getElementsByTagName("name")->item(0)->textContent;
-							}
-						}
-
-						array_push($gatheredFiles, $pathCheckFunc($file));
+				if($tagAttribute = $tag->getAttribute("helper")) {
+					$file = $this->gatherHelperInfo($tagAttribute);
+				} else if($tagValue = $tag->getElementsByTagName("type")) {
+					if(strpos($tagValue->item(0)->textContent, "skin") === false) {
+						$file = $tag->getElementsByTagName("name")->item(0)->textContent;
 					}
 				}
 
-				$strangeJSFiles = $xmlParser->searchForNodesByName("file"); // In hope for a better variable name
-				foreach($strangeJSFiles as $strangeJSFile) {
-					array_push($gatheredFiles, $pathCheckFunc($strangeJSFile->textContent));
-				}
+				array_push($gatheredFiles, $pathCheckFunc($file));
 			}
+		}
+
+		$strangeJSFiles = $xmlParser->searchForNodesByName("file"); // In hope for a better variable name
+		foreach($strangeJSFiles as $strangeJSFile) {
+			array_push($gatheredFiles, $pathCheckFunc($strangeJSFile->textContent));
 		}
 
 		return $gatheredFiles;
@@ -169,40 +178,63 @@ class SPExtensionController {
 	/**
 	 * Gathers paths of files found by node in an array of layout files
 	 *
-	 * @param array $areaLayoutFiles
+	 * @param SPXMLParser $xmlParser
+	 * @param string $areaKey
 	 * @param array $nodeNames
 	 * @param array $attributeNames
 	 *
 	 * @return string[]
 	 */
-	private function gatherPathsFromLayoutFiles($areaLayoutFiles, $nodeNames, $attributeNames) {
-		$xmlParser             = new SPXMLParser();
+	private function gatherPathsFromLayoutFiles($xmlParser, $areaKey, $nodeNames, $attributeNames) {
 		$foundPaths      = array(
-			"frontend"  => array(),
-			"adminhtml" => array()
+			$areaKey  => array()
 		);
 
-		foreach($areaLayoutFiles as $areaKey => $layoutFiles) {
-			foreach ( $layoutFiles as $layoutFile ) {
-				$xmlParser->load( $layoutFile );
+		foreach ( $attributeNames as $attributeName ) {
+			$attributeNodes = $xmlParser->searchForNodesByAttribute( $attributeName );
+			$nodeValues     = SPXMLParser::getAttributeContentFromNodes( $attributeNodes, $attributeName );
+			$foundPaths[$areaKey] = array_merge( $nodeValues, $foundPaths[$areaKey] );
+		}
 
-				foreach ( $attributeNames as $attributeName ) {
-					$attributeNodes = $xmlParser->searchForNodesByAttribute( $attributeName );
-					$nodeValues     = SPXMLParser::getAttributeContentFromNodes( $attributeNodes, $attributeName );
-					$foundPaths[$areaKey] = array_merge( $nodeValues, $foundPaths[$areaKey] );
-				}
-
-				foreach ( $nodeNames as $nodeName ) {
-					$nameNodes  = $xmlParser->searchForNodesByName( $nodeName );
-					$nodeValues = SPXMLParser::getTextContentFromNodes( $nameNodes );
-					$foundPaths[$areaKey] = array_merge($nodeValues, $foundPaths[$areaKey]);
-				}
-			}
+		foreach ( $nodeNames as $nodeName ) {
+			$nameNodes  = $xmlParser->searchForNodesByName( $nodeName );
+			$nodeValues = SPXMLParser::getTextContentFromNodes( $nameNodes );
+			$foundPaths[$areaKey] = array_merge($nodeValues, $foundPaths[$areaKey]);
 		}
 
 		return $foundPaths;
 	}
 
+	/**
+	 * Iterates through layout files and expects a lambda function
+	 * which a xml parser, the area key, and a output variable is provided as parameters
+	 *
+	 * @param string[] $areaLayoutFiles
+	 * @param Closure $lambda
+	 * @param mixed $outputType
+	 *
+	 * @return array
+	 */
+	private function loadLayoutFiles($outputType, $areaLayoutFiles, $lambda) {
+		$xmlParser = new SPXMLParser();
+
+		foreach($areaLayoutFiles as $areaKey => $layoutFiles) {
+			foreach ( $layoutFiles as $layoutFile ) {
+				$xmlParser->load($layoutFile);
+				$lambda($xmlParser, $areaKey, $outputType);
+			}
+		}
+
+		return $outputType;
+	}
+
+	/**
+	 * Gathers data which is in a magento helper object
+	 *
+	 * @param string $helperInfo
+	 *
+	 * @return string
+	 */
 	private function gatherHelperInfo( $helperInfo ) {
 		$helperValeArray = explode( "/", $helperInfo );
 		$count           = count( $helperValeArray );
